80ed8709cb368abd48366cb472770189b1a4d44e
root <root@bsmith-laptop.(none)>
2013-03-16 04:32:00 -0400

    TSK-5: updating project depends

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,23 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      // Add your Project dependencies here,
+      filters,
+      jdbc,
+      "com.typesafe" % "play-slick_2.10" % "0.3.0",
+      "com.wordnik" %% "swagger-play2" % "1.2.1-SNAPSHOT",
+      "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-      // Add your own Project settings here      
+    val main = play.Project(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
+      testOptions in Test := Nil,
+      resolvers := Seq(
+        "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+        "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/",
+        "Maven Central" at "http://repo1.maven.org/maven2/",
+        "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+         Resolver.url("Play Slick@ Github", url("http://loicdescotte.github.com/releases/"))(Resolver.ivyStylePatterns)
+      )
     )
 
 }
