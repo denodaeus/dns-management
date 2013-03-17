5f73b5e2273070f184339ee65e688625de049acb
Bobby Smith <bobby.smith@gmail.com>
2013-03-17 16:24:08 -0400

    TSK-11: refactor domains

--- app/controllers/Domains.scala
+++ app/controllers/Domains.scala
@@ -1,81 +1,115 @@
 package controllers
 
-import models.Domain
-import data.DataStore
+import scala.util.Failure
+import scala.util.Success
 
+import models.Domain
+import models.DomainForCreate
+import models.DomainForUpdate
+import models.DomainId
+import play.Logger
 import play.api.data.Form
-import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
-import play.api.data.Forms._
-import play.api.data.format.Formats._
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import play.api.mvc._
-import play.api.i18n.Messages
+import play.api.data.Forms.longNumber
+import play.api.data.Forms.mapping
+import play.api.data.Forms.nonEmptyText
+import play.api.data.Forms.number
+import play.api.data.Forms.optional
+import play.api.libs.functional.syntax.functionalCanBuildApplicative
+import play.api.libs.functional.syntax.toContraFunctorOps
+import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.json.Json
+import play.api.libs.json.Json.toJsFieldJsValueWrapper
+import play.api.mvc.Action
+import play.api.mvc.Controller
 
 object Domains extends Controller {
-
-  implicit object DomainFormat extends Format[Domain] {
-    def writes(d: Domain): JsValue = JsObject(
-      List("id" -> JsNumber(d.id),
-          "name" -> JsString(d.name),
-          "master" -> JsString(d.master),
-          "lastCheck" -> JsNumber(d.lastCheck),
-          "type" -> JsString(d.domainType),
-          "notifiedSerial" -> JsNumber(d.notifiedSerial),
-          "account" -> JsString(d.account)
-      )
-    )
-    
-    def reads(json: JsValue): JsResult[Domain] = JsSuccess(Domain(
-      (json \ "id").as[Int],
-      (json \ "name").as[String],
-      (json \ "master").as[String],
-      (json \ "lastCheck").as[Long],
-      (json \ "type").as[String],
-      (json \ "notifiedSerial").as[Long],
-      (json \ "account").as[String]
-    ))
-  }
+  import play.api.Play.current
   
-  val domain = new Domain(1, "accounts.vocalocity.com", "atlsvppdns01.atldc.vocalocity.com", 2123145525, "SLAVE", 600, "Route53")
-  val domain2 = new Domain(2, "media.vocalocity.com", "atlsvppdns01.atldc.vocalocity.com", 2123145525, "SLAVE", 600, "PowerDNS")
-  var db = DataStore.domains
-  db.put(domain.id, domain)
-  db.put(domain2.id, domain2)
+  implicit val writes = Json.writes[Domain]
+  
+  val domainForm = Form(
+    mapping(
+      "id" -> optional(number),
+      "name" -> nonEmptyText,
+      "master" -> nonEmptyText,
+      "lastCheck" -> longNumber,
+      "domainType" -> nonEmptyText,
+      "notifiedSerial" -> longNumber,
+      "account" -> nonEmptyText
+    ) (Domain.apply)(Domain.unapply)
+  )
   
   def listAll = Action { implicit request =>
-    var array = Json.arr(domain, domain2)
-    val json = Json.arr("GET /domains -> Domains.listAll", array)
-    Ok(json) as JSON
-  }
-  
-  def create = Action { implicit request =>
-    val json = Json.arr("POST /domains -> Domains.create", domain, domain2)
-    Ok(json) as JSON
-  }
-  
-  def updateAll = Action { implicit request =>
-    val json = Json.arr("PUT /domains -> Domains.updateAll", domain, domain2)
-    Ok(json) as JSON
-  }
-  
-  def deleteAll = Action { implicit request =>
-    val json = Json.arr("DELETE /domains -> Domains.deleteAll", domain, domain2)
+    val json = Json.toJson(Domain.findAll.map(d => Json.toJson(d)))
     Ok(json) as JSON
   }
   
   def get(id: Int) = Action { implicit request =>
-    val json = Json.arr(s"GET /domains/$id -> Domains.get $id", db.get(id))
+    Domain.findById(id) match {
+      case Success(d) => {
+        Ok(Json.toJson(d))
+      }
+      case Failure(e) => NotFound
+    }
+  }
+  
+  def create = Action { implicit request =>
+    implicit val writes = Json.writes[DomainId]
+    implicit val reads = Json.reads[DomainForCreate]
+    request.body.asJson.map { json =>
+      json.validate[DomainForCreate].fold(
+        invalid => {
+          BadRequest(Json.toJson(Map("error" -> invalid.head.toString)))
+        },
+        valid => {
+          Domain.create(valid) match {
+            case Success(r) => {
+              Ok(Json.toJson(r))
+            }
+            case Failure(e) => BadRequest(Json.toJson(Map("error" -> e.getMessage)))
+          }
+        })
+    }.getOrElse(BadRequest (Json.toJson(Map("status" -> "error request.body", "message" -> "Content Type Not Json"))))
+  }
+  
+  def updateAll = Action { implicit request =>
+    val json = Json.arr("PUT /domains -> Domains.updateAll UNIMPLEMENTED")
+    Ok(json) as JSON
+  }
+  
+  def deleteAll = Action { implicit request =>
+    val count = Domain.deleteAll
+    Logger.debug(s"deleteAll :: deleted all records, count=$count")
+    val json = Json.arr()
     Ok(json) as JSON
   }
   
   def updateIfExists(id: Int) = Action { implicit request =>
-    val json = Json.arr(s"PUT /domains/$id -> Domains.updateIfExists $id", domain)
-    Ok(json) as JSON
+    implicit val writes = Json.writes[DomainId]
+    implicit val reads = Json.reads[DomainForUpdate]
+    request.body.asJson.map { json =>
+      json.validate[DomainForUpdate].fold(
+        invalid => {
+          BadRequest(Json.toJson(Map("error" -> invalid.head.toString)))
+        },
+        valid => {
+          Domain.update(valid) match {
+            case Success(r) => {
+              Ok (Json.toJson(DomainId(valid.id)))
+            }
+            case Failure(e) => {
+              Logger.error(s"updateIfExists :: error updating domain $id, cause=", e)
+              BadRequest(Json.toJson(Map("error" -> e.getMessage)))
+            }
+          }
+        }
+      )
+    }.getOrElse(BadRequest (Json.toJson(Map("status" -> "error request.body", "message" -> "Content Type Not Json"))))
   }
   
   def deleteOne(id: Int) = Action { implicit request =>
-    val json = Json.arr(s"DELETE /domains/$id -> Domains.deleteOne $id", domain)
+    val deleted = Domain.delete(id)
+    val json = Json.arr(s"DELETE /domain/$id -> Domain.deleteOne $id")
     Ok(json) as JSON
   }
-}
+}  
--- app/models/Domain.scala
+++ app/models/Domain.scala
@@ -2,9 +2,17 @@ package models
 
 import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick.Profile
+import models.Domain.DomainTable
+import play.api.db.slick.Config.driver.simple._
+import play.api.db.slick.DB
+import play.Logger
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import scala.language.reflectiveCalls
+import scala.util.Try
 
 case class Domain (
-    id: Int,
+    id: Option[Int] = None,
     name: String,
     master: String,
     lastCheck: Long,
@@ -13,23 +21,86 @@ case class Domain (
     account: String
 )
 
+case class DomainId(id: Int)
+case class DomainForCreate(name: String, master: String, domainType: String, notifiedSerial: Long, account: String)
+case class DomainForUpdate(id: Int, master: String, lastCheck: Long, domainType: String, account: String)
+case class DomainForRead(id: Int, name: String, master: String, lastCheck: Long, domainType: String, notifiedSerial: Long, account: String)
+
 object DomainType extends Enumeration {
   type Type = Value
   val MASTER, SLAVE, NATIVE = Value
 }
 
-object Domain extends DomainDAO {
-}
-
-trait DomainDAO {
-  object DomainTable extends Table[Domain]("domain") {
+object Domain {
+  import play.api.Play.current
+  
+  object DomainTable extends Table[Domain]("domains") {
     def id = column[Int]("id", O.AutoInc, O.PrimaryKey, O.NotNull)
     def name = column[String]("name", O.NotNull)
     def master = column[String]("master", O.Default(null))
     def lastCheck = column[Long]("last_check")
     def domainType = column[String]("type", O.NotNull)
     def notifiedSerial = column[Long]("notified_serial")
-    def account = column[String]("provider", O.Default(null))
-    def * = id ~ name ~ master ~ lastCheck ~ domainType ~ notifiedSerial ~ account <> (Domain.apply _, Domain.unapply _)
+    def account = column[String]("account", O.Default(null))
+    def * = id.? ~ name ~ master ~ lastCheck ~ domainType ~ notifiedSerial ~ account <> (Domain.apply _, Domain.unapply _)
+    def autoInc = * returning id
+    
+    def findAll(implicit session: Session) = (for (d <- this) yield d).list
+    def findById(id: Int)(implicit session: Session) = createFinderBy(_.id).first(id)
+    def delete(id: Int)(implicit session: Session) = this.where(_.id === id).mutate(_.delete)
+    def deleteAll(implicit session: Session) = (for (d <- this) yield d).delete
+    def forInsert = name ~ master ~ lastCheck ~ domainType ~ notifiedSerial ~ account <>
+      ({ (name, master, lastCheck, domainType, notifiedSerial, account) => Domain(None, name, master, lastCheck, domainType, notifiedSerial, account)},
+          {d: Domain => Some((d.name, d.master, d.lastCheck, d.domainType, d.notifiedSerial, d.account)) }) returning id
   }
+
+  def findById (id: Int): Try[Domain] = DB.withSession { implicit session =>
+    Logger.debug(s"findById :: finding by Id=$id")
+    Try(DomainTable.findById(id))
+  }
+  
+  def findAll: List[Domain] = {
+    Logger.debug("find: finding " )
+    val domains = DB.withSession { implicit session =>
+      DomainTable.findAll
+    }
+    domains
+  }
+  
+  def create(domain: DomainForCreate): Try[DomainId] = DB.withSession { implicit session =>
+    Logger.debug("create for domain " + domain + " at time " + nowInUnixTime)
+    Try(
+      DomainId(
+        DomainTable.forInsert.insert(
+          Domain(None,
+            domain.name,
+            domain.master,
+            nowInUnixTime,
+            domain.domainType,
+            domain.notifiedSerial,
+            domain.account
+          ))))
+  }
+    
+  def update(domain: DomainForUpdate): Try[Unit] = DB.withSession { implicit session =>
+    Logger.debug("update :: updating for domain " + domain + " with id=" + domain.id)
+    Try((DomainTable.filter(d => d.id === domain.id).map(d => d.master ~ d.lastCheck ~ d.domainType ~ d.account))
+        .update(domain.master, nowInUnixTime, domain.domainType, domain.account))
+  }
+  
+  def delete(id: Int) = {
+    val deleted = DB.withSession { implicit session =>
+      DomainTable.delete(id)
+    }
+    deleted
+  }
+  
+  def deleteAll = {
+    val deleted = DB.withSession { implicit session =>
+      DomainTable.deleteAll
+    }
+    deleted
+  }
+  
+  def nowInUnixTime = { (System.currentTimeMillis / 1000L).toInt }  
 }
