e420da60541aacaf0314a3074f2983712aae0ccb
Bobby Smith <bobby.smith@gmail.com>
2013-03-31 17:52:46 -0400

    TSK-45: commiting models methods

---
+++ app/models/Models.scala
@@ -0,0 +1,12 @@
+package models
+
+import play.api.db.DB
+import play.api.Play.current
+
+import scala.slick.session.Database
+import scala.slick.session.Database.threadLocalSession
+
+case class Page[A](items: List[A], page: Int, offset: Long, total: Long) {
+	lazy val prev = Option(page - 1).filter(_ >= 0)
+	lazy val next = Option(page + 1).filter(_ => (offset + items.size) < total)
+}
