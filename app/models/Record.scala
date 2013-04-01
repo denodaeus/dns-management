69941a9860c9ef858e816b607166daa85bffc64a
Bobby Smith <bobby.smith@gmail.com>
2013-04-01 17:40:50 -0400

    TSK-46: formatting

--- app/models/Record.scala
+++ app/models/Record.scala
@@ -35,72 +35,72 @@ object Records extends Table[Record]("records"){
   
   lazy val pageSize = 20
 
-    def id = column[Int]("id", O.AutoInc, O.PrimaryKey)
-    def domainId = column[Int]("domain_id", O.NotNull)
-    def name = column[String]("name", O.Default(null))
-    def recordType = column[String]("type", O.Default(null))
-    def content = column[String]("content", O.Default(null))
-    def ttl = column[Int]("ttl")
-    def priority = column[Int]("prio")
-    def changeDate = column[Int]("change_date")
-    def accountId = column[Int]("account_id")
-    def domainFK = foreignKey("domain_exists", domainId, DomainTable)(_.id)
-    def * = id.? ~ domainId ~ name ~ recordType ~ content ~ ttl ~ priority ~ changeDate ~ accountId <> (Record, Record.unapply _)    
-    def autoInc = id.? ~ domainId ~ name ~ recordType ~ content ~ ttl ~ priority ~ changeDate ~ accountId <> (Record, Record.unapply _) returning id
+  def id = column[Int]("id", O.AutoInc, O.PrimaryKey)
+  def domainId = column[Int]("domain_id", O.NotNull)
+  def name = column[String]("name", O.Default(null))
+  def recordType = column[String]("type", O.Default(null))
+  def content = column[String]("content", O.Default(null))
+  def ttl = column[Int]("ttl")
+  def priority = column[Int]("prio")
+  def changeDate = column[Int]("change_date")
+  def accountId = column[Int]("account_id")
+  def domainFK = foreignKey("domain_exists", domainId, DomainTable)(_.id)
+  def * = id.? ~ domainId ~ name ~ recordType ~ content ~ ttl ~ priority ~ changeDate ~ accountId <> (Record, Record.unapply _)    
+  def autoInc = id.? ~ domainId ~ name ~ recordType ~ content ~ ttl ~ priority ~ changeDate ~ accountId <> (Record, Record.unapply _) returning id
 
-    val byId = createFinderBy(_.id)
-    val byDomainId = createFinderBy(_.domainId)
-    val byName = createFinderBy(_.name)
-    val byContent = createFinderBy(_.content)
-    val byAccountId = createFinderBy(_.accountId)
+  val byId = createFinderBy(_.id)
+  val byDomainId = createFinderBy(_.domainId)
+  val byName = createFinderBy(_.name)
+  val byContent = createFinderBy(_.content)
+  val byAccountId = createFinderBy(_.accountId)
     
-    def findAll = DB.withSession { 
-      implicit session =>
-        (for (r <- Records.sortBy(_.id) ) yield r).list 
-    }
+  def findAll = DB.withSession { 
+    implicit session =>
+      (for (r <- Records.sortBy(_.id) ) yield r).list 
+  }
     
-    def findById(id: Int) = DB.withSession {
-      implicit session =>
-        Try(Records.byId(id).firstOption) 
-    }
+  def findById(id: Int) = DB.withSession {
+    implicit session =>
+      Try(Records.byId(id).firstOption) 
+  }
     
-    def findByAccountId(accountId: Int) = DB.withSession { 
-      implicit session => 
-        Try(Records.byAccountId(accountId).list) 
-    }
+  def findByAccountId(accountId: Int) = DB.withSession { 
+    implicit session => 
+      Try(Records.byAccountId(accountId).list) 
+  }
     
-    def findByDomainId(domainId: Int) = DB.withSession {
-      implicit session =>
-        Try(Records.byDomainId(domainId).list) 
-    }
+  def findByDomainId(domainId: Int) = DB.withSession {
+    implicit session =>
+      Try(Records.byDomainId(domainId).list) 
+  }
     
-    def listAccountIds = DB.withSession { 
-      implicit session => 
-        (for (r <- Records) yield r.accountId).list.distinct  
-    }
+  def listAccountIds = DB.withSession { 
+    implicit session => 
+      (for (r <- Records) yield r.accountId).list.distinct  
+  }
     
-    def delete(id: Int) = DB.withSession { 
-      implicit session => 
-        Try(Records.where(_.id === id).delete) 
-    }
+  def delete(id: Int) = DB.withSession { 
+    implicit session => 
+      Try(Records.where(_.id === id).delete) 
+  }
     
-    def deleteAll = DB.withSession { 
-      implicit session => 
-        Try((for (r <- Records) yield r).delete)
-    }
+  def deleteAll = DB.withSession { 
+    implicit session => 
+      Try((for (r <- Records) yield r).delete)
+  }
     
-    def insert(record: Record) = DB.withSession { 
-      implicit session => 
-        Try(Records.autoInc.insert((record)))
-    }
+  def insert(record: Record) = DB.withSession { 
+    implicit session => 
+      Try(Records.autoInc.insert((record)))
+  }
     
-    def update(id: Int, record: Record) = DB.withSession { 
-      implicit session => {
-        val recordToUpdate = record.copy(Some(id), record.domainId, record.name, record.recordType, record.content, record.ttl, record.priority, nowInUnixTime, record.accountId)
-        Logger.debug("update: updating recordid=" + id + ", record=" + record + ", previous=" + Records.findById(id))
-        Try(Records.where(_.id === id).update(recordToUpdate))
-      }
+  def update(id: Int, record: Record) = DB.withSession { 
+    implicit session => {
+      val recordToUpdate = record.copy(Some(id), record.domainId, record.name, record.recordType, record.content, record.ttl, record.priority, nowInUnixTime, record.accountId)
+      Logger.debug("update: updating recordid=" + id + ", record=" + record + ", previous=" + Records.findById(id))
+      Try(Records.where(_.id === id).update(recordToUpdate))
     }
+  }
 
   def findPage(page: Int = 0, orderField: Int): Page[Record] = {
     val offset = pageSize * page
