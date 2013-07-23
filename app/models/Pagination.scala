96e34afc3f1442cc99d80bed534f4606dcf3df11
Bobby Smith <bobby.smith@gmail.com>
2013-07-23 16:42:59 -0400

    PT-445: add pagination

---
+++ app/models/Pagination.scala
@@ -0,0 +1,16 @@
+package models
+
+import play.api.db.DB
+import play.api.Play.current
+import play.api.mvc.QueryStringBindable
+
+import scala.slick.session.Database
+import scala.slick.session.Database.threadLocalSession
+
+case class Page[+A](items: Seq[A], page: Int, offset: Long, total: Long, pageSize: Int) {
+  lazy val prev = Option(page - 1).filter(_ >= 0)
+  lazy val next = Option(page + 1).filter(_ => (offset + items.size) < total)
+  lazy val maxPages = (total.toDouble / pageSize).ceil.toInt
+  lazy val paginationStart = (page - 2).max(1)
+  lazy val paginationEnd = (page + 3).min(maxPages)
+}
